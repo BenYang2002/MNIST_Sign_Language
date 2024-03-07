203232d62d63787b807135b8db435a6558fa3d8c
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2024-03-07 12:13:20 -0800

    Update FinalProject.m

--- FinalProject.m
+++ FinalProject.m
@@ -1,10 +1,7 @@
 training_data = readmatrix('train.csv');
+network = MultiLayerNetwork(2, [784, 100, 100, 26]);
 test_data = readmatrix('test.csv');
-network = MultiLayerNetwork(2, [784, 30, 30, 26]);
-
-training_data = training_data(1:20000,:);
-test_data = training_data(20001:end,:);
-
+test_data2 = training_data(25001:end,:);
 Train(network, training_data);
 
 res = Test(network, test_data);
@@ -12,6 +9,13 @@ res = Test(network, test_data);
 id = res(:, 1);
 label = res(:, 2);
 
+res2 = Test(network, test_data2);
+id2 = res2(:, 1);
+label2 = res2(:, 2);
+expected = test_data2(:,2);
+accuracy = calAccuracy(expected,label2);
+disp("accuracy");
+disp(accuracy);
 T = table(id, label);
 writetable(T, 'CSS485InterimAttempt.csv');
 type CSS485InterimAttempt.csv
