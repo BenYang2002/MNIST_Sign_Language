c08fe0e6e63e8956923be177130e68af81e78e04
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2024-03-06 17:54:57 -0800

    Update FinalProject.m
    devide the training to be training and testing

--- FinalProject.m
+++ FinalProject.m
@@ -2,6 +2,8 @@ training_data = readmatrix('train.csv');
 test_data = readmatrix('test.csv');
 network = MultiLayerNetwork(2, [784, 30, 30, 26]);
 
+training_data = training_data(1:20000,:);
+test_data = training_data(20001:end,:);
 
 Train(network, training_data);
 
