1e17f42f6491abc569a5cc565c7b86b598db7813
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2024-03-06 22:41:45 -0800

    Update Train.m

--- Train.m
+++ Train.m
@@ -12,6 +12,7 @@ function Train(network, data)
         end
         xplots = [];
         yplots = [];
+        mse = 0;
         for i = 1:size(data, 1)
 
             % Get training vector and corresponding expected output
@@ -26,13 +27,20 @@ function Train(network, data)
             % Run forward to save results to use in backprop,
             % compute error and save it
             a = forward(network, input);
+            disp("targer " + size(target));
+            disp("a " + size(a));
             e = target - a;
             disp("targer " + target);
             disp("a " + a);
             pIndex = e'*e;
-            xplots = [xplots,i];
-            yplots = [yplots,pIndex];
-            if (mod(i,size(data,1)/100))
+            mse = mse + pIndex;
+            if (mod(i,size(data,1)/100) == 0)
+                xplots = [xplots,i];
+                yplots = [yplots,mse/i];
+                if (mod(i,int32(size(data,1)/5)) == 0)
+                    xplots = [];
+                    yplots = [];
+                end
                 plotting(xplots,yplots);
                 drawnow();
             end
